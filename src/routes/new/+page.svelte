1185c535c4432c6c1cfaf381330eaff116ce4f34
Nukelimer <codewithremi@gmail.com>
2025-01-24 01:07:50 +0100

    ....

--- src/routes/new/+page.svelte
+++ src/routes/new/+page.svelte
@@ -1,5 +1,3 @@
-<div class=" flex my-auto justify-center items-center h-screen w-32">
-	<video autoplay class="video absolute inset-0  object-cover" muted="true" playsinline="true" webkit-playsinline="true" disableremoteplayback="true" loop=""
-		><source src="/video/key.mov" type="video/mp4; codecs=&quot;hvc1&quot;" /> <source src="/video/key.webm" type="video/webm" /></video
-	>
+<div class=" my-auto flex h-screen w-32 items-center justify-center">
+	<video autoplay class="video absolute inset-0 object-cover" muted="true" playsinline="true" webkit-playsinline="true" disableremoteplayback="true" loop=""><source src="/video/key.mov" type="video/mp4; codecs=&quot;hvc1&quot;" /> <source src="/video/key.webm" type="video/webm" /></video>
 </div>
