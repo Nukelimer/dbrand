47206ab1f071aa34f5023107cb6890a767950ae7
Nukelimer <codewithremi@gmail.com>
2025-01-31 12:01:04 +0100

    completed this route

---
+++ src/routes/shop/devices/galaxy-s25-series-skins/components/Parallax_S25.svelte
@@ -0,0 +1,33 @@
+<script>
+	export let section = [];
+
+          import { onMount } from "svelte";
+
+  let scrollY = 0;
+
+  const handleScroll = () => {
+    scrollY = window.scrollY;
+  };
+
+  onMount(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
+</script>
+
+{#each section as { text, image }, i}
+	<section class=" font-helvetica md:hidden ">
+		<!-- Background Image (shrinks in height) -->
+		<img src={image} alt="Parallax Background" class=" top-0 block box-border  w-full object-cover transition-transform sticky" />
+
+		<!-- Text content that moves over the image -->
+		<div class=" h- full py-16 top -0 block box-border w-full bg-black text-white sticky">
+			<div class="w-full py-10 px-6 bg-black ">
+				<p class="text-center font-helvetica  sm:text-lg">
+					{@html text[0]}
+				</p>
+				<h3 class=" text-center text-xl sm:text-start sm:text-xl">{text[1]}</h3>
+			</div>
+		</div>
+	</section>
+{/each}
