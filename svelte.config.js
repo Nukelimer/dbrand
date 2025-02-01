b8e4545f58471fe1f8f43b484cf44a02cb79ad5b
Nukelimer <codewithremi@gmail.com>
2025-02-01 19:22:07 +0100

    insrtalled verel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,9 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	kit: {
-		adapter: vercel()
+		adapter: adapter({
+			// see below for options that can be set here
+		})
 	}
 };
-
-export default config;
