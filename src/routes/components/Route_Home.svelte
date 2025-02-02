e940d240897b690721dbb9322f306f34d43b7c40
Nukelimer <codewithremi@gmail.com>
2025-02-02 22:59:35 +0100

    fixed the wrap

--- src/routes/components/Route_Home.svelte
+++ src/routes/components/Route_Home.svelte
@@ -1,32 +1,23 @@
 <script>
+	export let productData = [];
 
-
-       export let productData = [
-		
-	];
-
-        export let product=''
-
+	export let product = '';
 </script>
-
-
 
 <div class="h-full min-h-[100vh] bg-black text-white">
 	<div class="mx-auto px-6 pt-12 2xl:w-[1300px]">
-		<p class="text-lg text-[#FBBB29] px-4">dbrand for the</p>
-		<h1 class="font-helvetica text-6xl font-bold text-[#FBBB29] md:text-8xl px-4 ">{product}</h1>
+		<p class="px-4 text-lg text-[#FBBB29]">dbrand for the</p>
+		<h1 class="px-4 font-helvetica text-6xl font-bold text-[#FBBB29] md:text-8xl">{product}</h1>
 
-		<div class=" w-full mt-12                                                                                sm:gap-6 flex-col sm:flex sm:flex-row flex-wrap ">
+		<div class=" mt-12 h-full w-full flex-col flex-wrap sm:flex sm:flex-row sm:gap-6 md:flex-nowrap">
 			{#each productData as { img, name, perk, url }}
-				<div class=" mx-auto my-12 sm:my-0 max-w-[485px] max-h-[576px] overflow-hidden hover:rounded-t-md rounded-t-md rounded-b-md  ">
-					
-                                       <div class="overflow-hidden hover:rounded-t-md rounded-t-md  ">
-
-                                         <img src={img} alt={name} class="rounded-t-md hover:rounded-t-md object-cover  hover:scale-110 cursor-pointer delay-200 h-full  transition-transform" />
-                                       </div>
+				<div class=" mx-auto mt-12 max-h-[576px] max-w-[405px] overflow-hidden rounded-b-md rounded-t-md hover:rounded-t-md sm:my-0">
+					<div class="overflow-hidden rounded-t-md hover:rounded-t-md">
+						<img src={img} alt={name} class="h-full cursor-pointer rounded-t-md object-cover transition-transform delay-200 hover:scale-110 hover:rounded-t-md" />
+					</div>
 					<div class=" bg-[#292929] p-6">
-						<h3 class="pb-4 font-helvetica text-xl font-semibold ">{name}</h3>
-						<h4 class="uppercase">{perk}</h4>
+						<h3 class="pb-4 font-helvetica text-xl font-semibold">{name}</h3>
+						<h4 class="uppercase text-nowrap">{perk}</h4>
 					</div>
 					<a href={url} class="flex w-full justify-center rounded-b-md bg-[#FBBB29] p-2 text-center font-semibold uppercase text-black">Shop {name.split(' ').pop()}</a>
 				</div>
