b6236cb3f34e02560b0bde4608d5e508244b6f0a
Nukelimer <codewithremi@gmail.com>
2025-02-02 22:47:17 +0100

    16 route

---
+++ src/routes/components/Route_Home.svelte
@@ -0,0 +1,36 @@
+<script>
+
+
+       export let productData = [
+		
+	];
+
+        export let product=''
+
+</script>
+
+
+
+<div class="h-full min-h-[100vh] bg-black text-white">
+	<div class="mx-auto px-6 pt-12 2xl:w-[1300px]">
+		<p class="text-lg text-[#FBBB29] px-4">dbrand for the</p>
+		<h1 class="font-helvetica text-6xl font-bold text-[#FBBB29] md:text-8xl px-4 ">{product}</h1>
+
+		<div class=" w-full mt-12                                                                                sm:gap-6 flex-col sm:flex sm:flex-row flex-wrap ">
+			{#each productData as { img, name, perk, url }}
+				<div class=" mx-auto my-12 sm:my-0 max-w-[485px] max-h-[576px] overflow-hidden hover:rounded-t-md rounded-t-md rounded-b-md  ">
+					
+                                       <div class="overflow-hidden hover:rounded-t-md rounded-t-md  ">
+
+                                         <img src={img} alt={name} class="rounded-t-md hover:rounded-t-md object-cover  hover:scale-110 cursor-pointer delay-200 h-full  transition-transform" />
+                                       </div>
+					<div class=" bg-[#292929] p-6">
+						<h3 class="pb-4 font-helvetica text-xl font-semibold ">{name}</h3>
+						<h4 class="uppercase">{perk}</h4>
+					</div>
+					<a href={url} class="flex w-full justify-center rounded-b-md bg-[#FBBB29] p-2 text-center font-semibold uppercase text-black">Shop {name.split(' ').pop()}</a>
+				</div>
+			{/each}
+		</div>
+	</div>
+</div>
---
+++ src/routes/shop/catalog/iphone-16-series/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	import RouteHome from '../../../components/Route_Home.svelte';
+	const S25Data = [
+		{
+			img: '/images/catalog/16_skins.webp',
+			name: 'iPhone 16 Series Skins',
+			perk: 'Ultimate customization',
+			url: '/shop/devices/iphone-16-series-skins'
+                       
+		},
+		{
+			img: '/images/catalog/16_cases.webp',
+			name: 'iPhone 16 Series Cases',
+			perk: 'Revulutionary grip',
+			url: '/shop/devices/iphone-16-series-phone-cases'
+                      
+		},
+		{
+			img: '/images/catalog/16_glass.webp',
+			name: 'iPhone 16 Series Glass',
+			perk: 'save your screen',
+			url: '/shop/devices/iphone-16-series-tempered-glass-screen-protectors'
+		}
+	];
+</script>
+
+<RouteHome productData={S25Data} product={'iPhone 16 Series'} />
+
